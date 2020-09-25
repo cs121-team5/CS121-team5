2ed4dcd1105d37cbc2c1351e4da465d4d4120a15
Tats777 <tatsukikuze@gmail.com>
2020-09-25 11:06:10 +0800

    Hello World Added

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Hello World! Welcome to our CS121 homepage
         </a>
       </header>
     </div>
